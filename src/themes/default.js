e88bca0f560f00e35985917c51ca3493de613d85
Yogi-Codes <yogicodes101@gmail.com>
2023-11-06 11:03:31 +0530

    completed

---
+++ src/themes/default.js
@@ -0,0 +1,26 @@
+export default {
+  // Elegant fonts
+  fonts: {
+    title: "Open Sans, sans-serif",
+    main: "Lora, serif"
+  },
+  // Professional and Aesthetic Colors for Layout
+  colors: {
+    primary1: "#3E4095", // Deep Blue
+    background1: "#222222", // Dark Gray
+    button: "#3E4095", // Deep Blue
+    background2: "#333333", // Dark Gray
+    text: "#444444", // Light Gray
+    text1: "#666666", // Medium Gray
+    text2: "#999999", // Light Gray
+    text3: "#C8C8C8", // Very Light Gray
+    footerBackground: "#000000" // Black
+  },
+  // Responsive Design Breakpoints
+  breakpoints: {
+    sm: 'screen and (max-width: 640px)',
+    md: 'screen and (max-width: 768px)',
+    lg: 'screen and (max-width: 1024px)',
+    xl: 'screen and (max-width: 1280px)'
+  },
+}
